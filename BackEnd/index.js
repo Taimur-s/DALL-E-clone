7fca54f66c4d089de06ee9b95d38ff227e9740f7
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-24 00:08:51 +0500

    commit Backend

---
+++ BackEnd/index.js
@@ -0,0 +1,20 @@
+import express  from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+app.get('/', async (reqq,res) => {
+    res.send('Hello from DALL-E!')
+})
+
+const startServer = async () => {
+    app.listen(8080, () => console.log('Server has started on post http://localhost:8080'))
+}
+
+startServer();
