db2a650a32f05326e8d269b7eb1034739c641e8d
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-25 18:57:58 +0500

    finall commit

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -11,11 +11,12 @@ const RenderCards = ({data, title}) => {
   )
 }
 
-
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState (null);
   const [searchText, setsearchText] = useState('');
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
   useEffect(()=>{
     const fetchPosts = async () => {
       setLoading(true);
@@ -38,6 +39,20 @@ const Home = () => {
     }
     fetchPosts();
   },[]);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setsearchText(e.target.value);
+    setSearchTimeout(
+    setTimeout(()=>{
+      const searchResults = allPosts.filter((item)=> item.name.toLowerCase().includes(searchText.toLowerCase())||
+      item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+
+      setSearchedResults(searchResults);
+    }, 500)
+    );
+  }
+
   return (
     <section className='max=w=7xl mx-auto'>
       <div>
@@ -48,7 +63,14 @@ const Home = () => {
           </p>
       </div>
       <div className='mt-16'>
-        <FormField/>
+        <FormField
+        LabelName='search posts'
+        type='text'
+        name='text'
+        placeholder='Search posts'
+        value={searchText}
+        handleChange={handleSearchChange}
+        />
       </div>
 
       <div className='mt-10'>
@@ -67,8 +89,8 @@ const Home = () => {
           <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
             {searchText ? (
               <RenderCards
-              data={[]}
-              title='No earch results found'
+              data={searchedResults}
+              title='No search results found'
               />
             ):(
               <RenderCards
